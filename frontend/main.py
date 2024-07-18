ac185a72788641f47d260e23cf19bfaf3126f13a
[Gitau Guchu] <[gitaudennis59@gmail.com]>
2024-07-18 06:30:28 +0300

    My first commit

---
+++ frontend/main.py
@@ -0,0 +1,35 @@
+import requests
+import streamlit as st
+from PIL import Image
+
+STYLES = {
+    "candy": "candy",
+    "composition 6": "composition_vii",
+    "feathers": "feathers",
+    "la_muse": "la_muse",
+    "mosaic": "mosaic",
+    "starry night": "starry_night",
+    "the scream": "the_scream",
+    "the wave": "the_wave",
+    "udnie": "udnie",
+}
+
+st.set_option("deprecation.showfileUploaderEncoding", False)
+
+#Defines a hi header
+st.title("Style Transfer Web App")
+
+#Displays a file uploader widget
+image = st.file_uploader("Choose an image")
+
+#Displays the select widget for the styles
+style = st.selectbox("Choose the style", [i for i in STYLES.keys()])
+
+#Displays a button
+if st.button("Style Transfer"):
+    if image is not None and style is not None:
+        files = {"file": image.getvalue()}
+        res = requests.get(f"http://localhost:8080/{style}", files=files)
+        img_path = res.json()
+        image = Image.open(img_path.get("name"))
+        st.image(image, width=500)
